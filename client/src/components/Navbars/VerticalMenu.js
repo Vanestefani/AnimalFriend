4eb2c25262d8f87ef1e67dfd7482fe259e030138
vanessa <vanessa.estefania.corredor@gmail.com>
2020-05-14 02:19:32 -0500

    quitando enlases de menu vertical

--- client/src/components/Navbars/VerticalMenu.js
+++ client/src/components/Navbars/VerticalMenu.js
@@ -12,45 +12,36 @@ function VerticalMenu() {
       <Card className="card-general">
         <Container>
           <Nav className="flex-column" tag="nav">
-            <NavLink
-              className="active"
-          
-              onClick={(e) => e.preventDefault()}
-            >
-              <Link    to="/myperfil">              <img
-                src={usuario.fotoPerfil}
-                className="rounded-circle FotoUser "
-              ></img>
-
-              <p className="text-center">{usuario.nombre}</p>
+            <NavLink className="active" onClick={(e) => e.preventDefault()}>
+              <Link to="/myperfil">
+                <img
+                  src={usuario.fotoPerfil}
+                  className="rounded-circle FotoUser "
+                ></img>
+                <p className="text-center">{usuario.nombre}</p>
               </Link>
-
             </NavLink>
 
             <NavLink href="#pablo" onClick={(e) => e.preventDefault()}>
-              <Link to="/eventos" >
+              <Link to="/eventos">
                 <i className="fas fa-calendar-alt"></i>
                 Eventos
               </Link>
             </NavLink>
             <NavLink href="#pablo" onClick={(e) => e.preventDefault()}>
-              <Link to="/negocios" >
+              <Link to="/negocios">
                 <i className="fas fa-store-alt"></i>
                 Negocios
               </Link>
             </NavLink>
             <NavLink href="#pablo" onClick={(e) => e.preventDefault()}>
+
+              <Link to="/anuncios">
               <i className="fas fa-map-marker-alt"></i>
-              Animales perdidos
+                Anuncios
+              </Link>
             </NavLink>
-            <NavLink href="#pablo" onClick={(e) => e.preventDefault()}>
-              <i className="fas fa-house-damage"></i>
-              Adopciones
-            </NavLink>
-            <NavLink href="#pablo" onClick={(e) => e.preventDefault()}>
-              <i className="fas fa-heart"></i>
-              Emparejar
-            </NavLink>
+
             <NavLink href="#pablo" onClick={(e) => e.preventDefault()}>
               <i className="fas fa-book"></i>
               Normatividad
