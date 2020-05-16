16758ffff8bb48622c868c62d07d5dcac68333f4
vanessa <vanessa.estefania.corredor@gmail.com>
2020-05-16 18:39:06 -0500

    fix import user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-const User = require("../models/user");
+const User = require("../models/User");
 const Token = require("../models/token");
 const { sendEmail } = require("../utils/index");
 
--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -1,7 +1,7 @@
 const JwtStrategy = require('passport-jwt').Strategy,
     ExtractJwt = require('passport-jwt').ExtractJwt;
 
-const User = require('../models/user');
+const User = require('../models/User');
 
 const opts = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
